395a5f374e6227cd96937e565a23e44775f9da45
Rustam Rakhmatullov <rahmatullovri@gmail.com>
2024-11-26 23:23:06 +0400

    leetcode/medium/380. Insert Delete GetRandom O(1)

--- leetcode/medium/380. Insert Delete GetRandom O(1).go
+++ leetcode/medium/380. Insert Delete GetRandom O(1).go
@@ -1,30 +1,48 @@
 package medium
 
-type RandomizedSet struct {
-    rndMap map
-	rndSlice []int
-}
+import "math/rand"
 
+type RandomizedSet struct {
+	rndMap   map[int]int //Для хранения значения и его индекса в rndSlice
+	rndSlice []int       //Для рандомного доступа
+}
 
 func Constructor() RandomizedSet {
-    
+	return RandomizedSet{rndMap: make(map[int]int), rndSlice: []int{}}
 }
-
 
 func (this *RandomizedSet) Insert(val int) bool {
-    
+	if _, exists := this.rndMap[val]; exists {
+		return false
+	}
+	this.rndMap[val] = len(this.rndSlice)
+	this.rndSlice = append(this.rndSlice, val)
+	return true
 }
-
 
 func (this *RandomizedSet) Remove(val int) bool {
-    
-}
+	index, exists := this.rndMap[val]
+	if !exists {
+		return false
+	}
 
+	//Перемещаем последний элемент на место удалямого
+	lastElement := this.rndSlice[len(this.rndSlice)-1]
+	this.rndSlice[index] = lastElement
+	this.rndMap[lastElement] = index
+
+	//Удаляем последний элемент из слайса
+	this.rndSlice = this.rndSlice[:len(this.rndSlice)-1]
+
+	//Удаляем значение из мапы
+	delete(this.rndMap, val)
+
+	return true
+}
 
 func (this *RandomizedSet) GetRandom() int {
-    
+	return this.rndSlice[rand.Intn(len(this.rndSlice))]
 }
-
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
